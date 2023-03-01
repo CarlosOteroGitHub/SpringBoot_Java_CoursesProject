650ad91cdf0b61f275f94eacb5ba7102f7a6765c
carlos.otero <carlos.otero.r12@gmail.com>
2023-02-28 22:10:13 -0600

    springboot_project_28FEB_v2

---
+++ infraestructuradomain/gateway/src/main/java/com/backend/gateway/auxiliar/GlobalPostFiltering.java
@@ -0,0 +1,22 @@
+package com.backend.gateway.auxiliar;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Configuration
+public class GlobalPostFiltering {
+
+    @Bean
+    public GlobalFilter postGlobalFilter() {
+        return (exchange, chain) -> {
+            return chain.filter(exchange)
+                    .then(Mono.fromRunnable(() -> {
+                        log.info("Global Post Filter executed");
+                    }));
+        };
+    }
+}
---
+++ infraestructuradomain/gateway/src/main/java/com/backend/gateway/auxiliar/GlobalPreFiltering.java
@@ -0,0 +1,19 @@
+package com.backend.gateway.auxiliar;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class GlobalPreFiltering implements GlobalFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("Global prefilter executed");
+        return chain.filter(exchange);
+    }
+}
