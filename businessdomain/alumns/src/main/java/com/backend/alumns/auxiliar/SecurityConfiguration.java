b8f9d223e705b10262a11409bb9271b8f9e1725c
carlos.otero <carlos.otero.r12@gmail.com>
2023-03-01 12:55:04 -0600

    springboot_project_01MAR_v1

--- businessdomain/alumns/src/main/java/com/backend/alumns/auxiliar/SecurityConfiguration.java
+++ businessdomain/alumns/src/main/java/com/backend/alumns/auxiliar/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.backend.alumns.auxiliar;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,14 +11,20 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+    
+    private static final String[] NO_AUTH_LIST = {
+        "/swagger/index.html",
+        "/login"
+    };
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeHttpRequests()
-                .anyRequest()
-                .authenticated()
-                .and()
+                .authorizeHttpRequests((authz) -> authz
+                .antMatchers(NO_AUTH_LIST).permitAll()
+                .antMatchers(HttpMethod.POST, "/**").authenticated()
+                .antMatchers(HttpMethod.PATCH, "/**").authenticated()
+                .antMatchers(HttpMethod.DELETE, "/**").authenticated())
                 .httpBasic(withDefaults())
                 .formLogin(withDefaults());
         return http.build();
